42dad9091695c4a95374e8babceea466cf313334
Olesia2001 <olesyakamasheva@gmail.com>
2026-09-17 14:49:32 +0400

    Fix CI driver indentation

--- conftest.py
+++ conftest.py
@@ -12,12 +12,11 @@ from pathlib import Path
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
-    
     if os.getenv("CI") == "true":
-    options.add_argument("--headless=new")
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--headless=new")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
     options.add_experimental_option(
         "prefs",
