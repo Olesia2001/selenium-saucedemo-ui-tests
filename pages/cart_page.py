ec1356561b8f529ef270b2080605ddbb9a301ba3
Olesia2001 <olesyakamasheva@gmail.com>
2026-09-17 15:00:23 +0400

    Handle stale cart elements

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -2,13 +2,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from pages.locators import CartPageLocators
+from selenium.common.exceptions import StaleElementReferenceException
 
 
 class CartPage:
-    def __init__(self,driver):
+    def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver,10)
+        self.wait = WebDriverWait(
+            driver,
+            10,
+            ignored_exceptions=(
+                StaleElementReferenceException,
+            )
+        )
 
+    
     def wait_until_opened(self):
         self.wait.until(
             EC.url_contains('cart.html')
@@ -33,6 +41,10 @@ class CartPage:
         )
         remove_button.click()
 
+        self.wait.until(
+            EC.staleness_of(remove_button)
+        )
+
 
     def get_item_count(self):
         item_elements = self.driver.find_elements(
@@ -47,7 +59,7 @@ class CartPage:
             )
         )
         checkout_button.click()
-        
+
             
 
 
